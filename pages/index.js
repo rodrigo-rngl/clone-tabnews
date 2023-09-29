db294c4bce2e5de08cf13feae752a58f2a566562
Rodrigo Rangel <rodrigo.rngl@hotmail.com>
2023-09-29 15:04:44 -0300

    change project for tailana homage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function Home(){
-    return <h1>TE AMO MINHA INDIAZINHA!!!</h1>
+    return <center style="margin-left: 30%; margin-right: 30%;">
+    <h1>Dra. Tailana,</h1>
+    <p>
+        Quero aproveitar esses últimos momentos para reconhecer o incrível prazer que tive em conhecer você e compartilhar momentos especiais aqui no escritório. Além disso, quero te desejar uma vida repleta de bênçãos, prosperidade e sucesso.
+    </p>
+    <p>
+        Durante esta semana, pedi a Deus que a protegesse e derramasse sobre você a graça necessária para que seja completamente feliz. Que seu propósito e seus dons possam servir para melhorar a vida de centenas de pessoas, tornando você uma oferta agradável a Deus. Estou ansioso para testemunhar todas as grandes realizações que estão por vir. Continue brilhando!
+    </p>
+    <p> Segue mais um mimo do seu anjo: </p>
+
+    <img src="3D_Tailana.jpg"></img>
+</center>
 }
 
 export default Home;
